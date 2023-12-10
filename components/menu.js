47b5128d1c09ca71477f1129b4717212c5ee657b
Parking Swing <148919566+parkingswing2323@users.noreply.github.com>
2023-12-10 13:54:08 +0100

    Update menu.js

--- components/menu.js
+++ components/menu.js
@@ -32,7 +32,7 @@ const Menu = () => {
   }, []);
 
   const onDeveloperClick = useCallback(() => {
-    window.open("https://jeffspace.vercel.app");
+    window.open("https://parkingswing.vercel.app");
   }, []);
 
   return (
